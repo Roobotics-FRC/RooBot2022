69aec2d636b9f1feb7ddaa63b576d999e925f4a2
Ben <34463636+Benji123abc@users.noreply.github.com>
2022-02-08 16:10:14 -0500

    Clean RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,115 +9,36 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 public class RobotMap {
 
+    // Button Ids
+    public static final int TEST_SHOOTER = 11;
+    public static final int CAMERA_PROCESS_BUTTON = 12;
+    public static final int DRIVE_TURN_90_BUTTON = 6;
+    public static final int DRIVE_SLOWER_SPEED_BUTTON = 2;
+
     // OI devices
     public static final int DRIVE_JOYSTICK_PORT = 0;
     public static final int OPERATOR_JOYSTICK_PORT = 1;
     public static final double JOYSTICK_DEFAULT_DEADZONE = 0.09;
+    public static final int PIGEON_ID = 19;
 
-    // Motors
-    public static final int DRIVETRAIN_MOTOR_RIGHT_1 = 10;
-    public static final int DRIVETRAIN_MOTOR_RIGHT_2 = 11;
-    public static final int DRIVETRAIN_MOTOR_RIGHT_3 = 12;
-    public static final int DRIVETRAIN_MOTOR_LEFT_1 = 13;
-    public static final int DRIVETRAIN_MOTOR_LEFT_2 = 14;
-    public static final int DRIVETRAIN_MOTOR_LEFT_3 = 15;
-    public static final boolean DRIVETRAIN_RIGHT_ENCODER_PHASE = false;
-    public static final boolean DRIVETRAIN_LEFT_ENCODER_PHASE = false;
+    // Motor Configs
+    public static final MotorConfig DRIVETRAIN_MOTOR_RIGHT_1 = new MotorConfig(10, true, false);
+    public static final MotorConfig DRIVETRAIN_MOTOR_RIGHT_2 = new MotorConfig(11, true, false);
+    public static final MotorConfig DRIVETRAIN_MOTOR_RIGHT_3 = new MotorConfig(12, true, false);
+    public static final MotorConfig DRIVETRAIN_MOTOR_LEFT_1 = new MotorConfig(13, false, false);
+    public static final MotorConfig DRIVETRAIN_MOTOR_LEFT_2 = new MotorConfig(14, false, false);
+    public static final MotorConfig DRIVETRAIN_MOTOR_LEFT_3 = new MotorConfig(15, false, false);
 
+    public static final MotorConfig SHOOTER_MOTOR_1 = new MotorConfig(21, false, true);
+    public static final MotorConfig SHOOTER_MOTOR_2 = new MotorConfig(22, false, false);
 
+    public static final PID DRIVETRAIN_ANG_PID_GAINS = new PID(0, 0.009, 0, 0.008);
 
-    // Buttons and axes
-    public static final int DRIVE_RESET_NORTH_BUTTON = 7;
-    public static final int DRIVE_TURN_90_BUTTON = 6;
-    /*
-    public static final int DRIVE_NORTH_UP_BUTTON = 10;
-    public static final int DRIVE_OWN_SHIP_UP_BUTTON = 12;
-    public static final int DRIVE_SLOWER_SPEED_BUTTON = 2;
-    public static final int DRIVE_DISABLE_ASSIST_BUTTON = 5;
-    public static final int DRIVE_CLEAR_COMMANDS_BUTTON = 11;
-    public static final int DRIVE_VISION_ALIGN_BUTTON = 6;
-    public static final int DRIVE_DISABLE_BRAKE_BUTTON = 1; // drive trigger
-    public static final int OPER_BALL_RELEASE_BUTTON = 5; // left bumper
-    public static final int OPER_INTAKE_BUTTON = 6; // right bumper
-    public static final int OPER_RAISE_L_WINCH_AXIS = 2; // L trigger
-    public static final int OPER_RAISE_R_WINCH_AXIS = 3; // R trigger
-    public static final int OPER_SHOOT_BUTTON = 4; // Y button
-    public static final int OPER_FALLBACK_SHOOT_BUTTON = 2; // B button
-    public static final int OPER_TOGGLE_SPINNER_BUTTON = 10; // right stick click
-    public static final int OPER_SPINNER_REVS_BUTTON = 7; // back button
-    public static final int OPER_SPINNER_COLOR_BUTTON = 8; // start button
-    public static final int OPER_REVERSE_INTAKE_BUTTON = 1; // A button
-    public static final int OPER_ADJUST_SHOOT_SPEED_AXIS = 1; // left stick Y
-    public static final int OPER_SHOOT_FROM_WALL_BUTTON = 3; // X button
-    */
-
-    // SET TO REAL BUTTON!
-    public static final int DRIVE_DISABLE_BRAKE_BUTTON = 1; // drive trigger
-    public static final int DRIVE_SLOWER_SPEED_BUTTON = 2; //thumb button
-    public static final int OPER_INTAKE_BUTTON = 3; // R trigger
-    public static final int OPER_INTAKE_DISLODGE_BUTTON = 6; // R bumper
-    public static final int OPER_INTAKE_RETRACT_BUTTON = 4;
-    public static final int OPER_INTAKE_DEPLOY_BUTTON = 2;
-    public static final int OPER_HOPPER_AXIS = 2; // L trigger
-    public static final int OPER_HOPPER_DISLODGE_BUTTON = 5; // L bumper
-    public static final int OPER_SHOOT_BUTTON = 1; // A button
-    public static final int OPER_FALLBACK_SHOOT_BUTTON = 3; // X button
-    public static final int OPER_WINCH_AXIS_X = 4;
-    public static final int OPER_WINCH_AXIS_Y = 5; // R stick Y
-    public static final int OPER_ADJUST_SHOOT_SPEED_AXIS = 1; // left stick Y
-
-
-    public static final double OPER_ROTATE_VIB_INTENSITY = 0.5;
-
-    // Speed constants
-    public static final double CLIMB_ARM_MOVE_SPEED = 0.25;
-    public static final double CLIMB_WINCH_MAX_SPEED = 0.8;
-    public static final double SHOOTER_MAX_SPEED_NATIVE_UNITS = 32500;
-    public static final double DRIVE_SLOWER_SPEED_FACTOR = 4;
-    public static final double AUTON_TURN_SPEED = 0.25;
-    public static final double AUTON_LINE_SHOOT_SPEED = 0.75;
-    public static final double AUTON_DRIVE_SPEED = 0.2;
-    public static final double GROUND_INTAKE_SPEED = 1;
-    public static final double UPTAKE_INTAKE_SPEED = 0.45;
-    public static final double DRIVE_ASSIST_MAX_TURN_SPEED = 0.2;
-    public static final double SHOOT_FROM_WALL_SPEED = 0.63;
-    public static final double INTAKE_DEPLOY_SPEED = 0.2;
-    public static final double INTAKE_RETRACT_SPEED = -0.4;
-    public static final double INTAKE_DRAW_SPEED = 0.5;
-    public static final double INTAKE_DISLODGE_SPEED = 0.15;
-    public static final double HOPPER_SPEED = 0.5;
-    public static final double HOPPER_DISLODGE_SPEED = 0.3;
-
-    // Non-motor devices
-    public static final int CLIMBER_ARM_RETRACTED_LIMIT_SWITCH = 1;
-    public static final int CLIMBER_ARM_EXTENDED_LIMIT_SWITCH = 0;
-    public static final int INTAKE_DEPLOYED_LIMIT_SWITCH = 2;
-    public static final int INTAKE_RETRACTED_LIMIT_SWITCH = 3;
-
-    // Physical state constants
-    public static final double DRIVE_WHEEL_DIAMETER_IN = 4;
-    public static final double DRIVE_GEARBOX_RATIO = 20 / 3d;
-    public static final double SHOOTER_TIME_TO_SPIN_UP_SEC = 1d;
-    // the below offset necessitates a CLOCKWISE (positive) rotation
-    public static final double INTER_CAMERA_SHOOTER_DIST_IN = 4.25;
-
-    // Talon constants
-    public static final int PID_IDX = 0;
+    // Constants
     public static final double DRIVE_ENCODER_COUNTS_PER_REV = 4096;
 
     // Conversion factors
-    public static final double ENCODER_UNITS_TO_INCHES = DRIVE_WHEEL_DIAMETER_IN * Math.PI
-            / DRIVE_ENCODER_COUNTS_PER_REV / DRIVE_GEARBOX_RATIO;
-
-    // Vision
-    public static final double VISION_SAMPLE_COUNT = 10;
-    public static final String VISION_TABLE_NAME = "Vision";
-    public static final String VISION_ANG_OFFSET_FIELD = "degree_offset";
-    public static final String VISION_DIST_FIELD = "current_distance";
-    public static final double VISION_ALIGN_ALLOWABLE_OFFSET_DEG = 1;
-    public static final double MAX_TURN_AUTON_TIME_SEC = 3;
-    public static final double INTER_QUERY_DELAY_SEC = 0.45;
-    public static final int MAX_ALLOWABLE_VISION_ITERATIONS = 4;
+    // public static final double ENCODER_UNITS_TO_INCHES = DRIVE_WHEEL_DIAMETER_IN * Math.PI / DRIVE_ENCODER_COUNTS_PER_REV / DRIVE_GEARBOX_RATIO;
 
     // Utility classes
     public static final class MotorConfig {
@@ -148,6 +69,20 @@ public class RobotMap {
          * Constructs a new MotorConfig for a motor not under closed-loop control.
          * @param id the CAN ID of the motor.
          * @param inverted whether to invert motor output values.
+         * @param encoderPhase whether the motor is out of phase with its sensor.
+         */
+        public MotorConfig(int id, boolean inverted, boolean encoderPhase) {
+            this.id = id;
+            this.inverted = inverted;
+            this.encoderPhase = encoderPhase;
+
+            this.neutralMode = NeutralMode.Brake;
+            this.gains = new PID(0, 0, 0,0);
+        }
+        /**
+         * Constructs a new MotorConfig for a motor not under closed-loop control.
+         * @param id the CAN ID of the motor.
+         * @param inverted whether to invert motor output values.
          * @param neutralMode the motor's neutral mode.
          */
         public MotorConfig(int id, boolean inverted, NeutralMode neutralMode) {
@@ -158,7 +93,6 @@ public class RobotMap {
             this.encoderPhase = false;
             this.gains = new PID(0, 0, 0,0);
         }
-
     }
 
     // PID- and sensor-related constants
@@ -182,8 +116,6 @@ public class RobotMap {
             this.kD = kD;
         }
     }
-
-    public static final PID DRIVETRAIN_ANG_PID_GAINS = new PID(0, 0.009, 0, 0.008);
 
     public static double constrainPercentOutput(double input) {
         return Math.max(Math.min(input, 1), -1);
